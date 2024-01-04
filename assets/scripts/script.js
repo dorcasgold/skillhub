a4fda75f16ecfa02891ad567dda9fcdbb0fb2eab
odetayo dorcas <odetayodorcas123@gmail.com>
2024-01-04 09:14:24 +0100

    hamburger menu & media query

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -27,3 +27,11 @@ var swiper = new Swiper(".slide-content", {
     },
   },
 });
+
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav')
+
+menu_btn.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+})
