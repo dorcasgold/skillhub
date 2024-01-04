6f2d2e3377d2e4a30c172654b9a1875d0c24b169
odetayo dorcas <odetayodorcas123@gmail.com>
2024-01-04 17:20:44 +0100

    preloader

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -35,3 +35,9 @@ menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
   mobile_menu.classList.toggle('is-active');
 })
+
+var loader = document.querySelector('.preloader')
+
+window.addEventListener('load', function () {
+  loader.style.display = 'none'
+})
