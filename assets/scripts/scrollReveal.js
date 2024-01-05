89f02dd788bfb9f57c5db5fd07878986fdd8d666
odetayo dorcas <odetayodorcas123@gmail.com>
2024-01-05 07:20:01 +0100

    ScrollReveal to elements

--- assets/scripts/scrollReveal.js
+++ assets/scripts/scrollReveal.js
@@ -19,9 +19,90 @@ ScrollReveal().reveal('.homepage_hero_img', {
 
 ScrollReveal().reveal('.job-section-img', {
   delay: 80,
+  duration: 2000,
+  opacity: 0,
+  distance: "50%",
+  origin: "bottom",
+  reset: true
+});
+
+ScrollReveal().reveal('.job-section-text', {
+  delay: 75,
+  duration: 1500,
+  opacity: 0,
+  distance: "50%",
+  origin: "right",
+  reset: true
+});
+
+ScrollReveal().reveal('.center-text', {
+  delay: 75,
+  duration: 1500,
+  opacity: 0,
+  distance: "50%",
+  origin: "top",
+  reset: true
+});
+
+ScrollReveal().reveal('.cards', {
+  delay: 75,
+  duration: 1500,
+  opacity: 0,
+  distance: "50%",
+  origin: "left",
+  reset: true
+});
+
+ScrollReveal().reveal('.flex-image', {
+  delay: 80,
+  duration: 2000,
+  opacity: 0,
+  distance: "50%",
+  origin: "right",
+  reset: true
+});
+
+ScrollReveal().reveal('.slide-container', {
+  delay: 80,
+  duration: 2000,
+  opacity: 0,
+  distance: "50%",
+  origin: "right",
+  reset: true
+});
+
+ScrollReveal().reveal('.Our-popular-courses', {
+  delay: 75,
+  duration: 1500,
+  opacity: 0,
+  distance: "50%",
+  origin: "top",
+  reset: true
+});
+
+ScrollReveal().reveal('.grid-image', {
+  delay: 75,
+  duration: 1500,
+  opacity: 0,
+  distance: "50%",
+  origin: "left",
+  reset: true
+});
+
+ScrollReveal().reveal('.footer', {
+  delay: 75,
   duration: 1500,
   opacity: 0,
   distance: "50%",
   origin: "bottom",
+  reset: true
+});
+
+ScrollReveal().reveal('.footer-bottom', {
+  delay: 75,
+  duration: 1500,
+  opacity: 0,
+  distance: "50%",
+  origin: "bottom left",
   reset: true
 });
