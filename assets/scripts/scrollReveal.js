d4fc0eab0db60726d8e1304c3fe3f6cac68ed8e0
odetayo dorcas <odetayodorcas123@gmail.com>
2024-01-05 06:54:30 +0100

    fix homepage_hero_text error

--- assets/scripts/scrollReveal.js
+++ assets/scripts/scrollReveal.js
@@ -1,3 +1,12 @@
+ScrollReveal().reveal('.homepage_hero_text', {
+  delay: 75,
+  duration: 1500,
+  opacity: 0,
+  distance: "50%",
+  origin: "left",
+  reset: true
+});
+
 ScrollReveal().reveal('.homepage_hero_img', {
   delay: 75,
   duration: 2000,
@@ -6,6 +15,7 @@ ScrollReveal().reveal('.homepage_hero_img', {
   origin: "bottom right",
   reset: true
 });
+
 
 ScrollReveal().reveal('.homepage_hero_text', {
   delay: 75,
